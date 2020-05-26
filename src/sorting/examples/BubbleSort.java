6aaba30c4bb31200e939aec6ed841643071e8df3
codeSmart2307 <raneesh2307@gmail.com>
2020-05-26 22:34:18 +0530

    add sorting

---
+++ src/sorting/examples/BubbleSort.java
@@ -0,0 +1,22 @@
+package sorting.examples;
+
+// Java program for implementation of Bubble Sort
+class BubbleSort {
+    void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    public static void main(String args[]) {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        for (int value : arr) System.out.print(value + " ");
+    }
+}
