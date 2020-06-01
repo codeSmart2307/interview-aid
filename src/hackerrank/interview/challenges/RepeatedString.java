ec0cf47cac51d69fab31b30230eb64c66b5a7c83
codeSmart2307 <raneesh2307@gmail.com>
2020-06-01 20:06:42 +0530

    add with bug on RepeatedString

--- src/hackerrank/interview/challenges/RepeatedString.java
+++ src/hackerrank/interview/challenges/RepeatedString.java
@@ -1,10 +1,32 @@
 package hackerrank.interview.challenges;
 
+import java.util.Collections;
+
 public class RepeatedString {
 
     // My original solution
     static long repeatedString(String s, long n) {
+        int stringLen = s.length();
+        String finalRepeatedString;
+        if (stringLen < n) {
+            long timesToRepeat = n/stringLen;
+            String fullStringRepeated = String.join("", Collections.nCopies(Math.toIntExact(timesToRepeat), s));
+            int nRemainder = (int)n - fullStringRepeated.length();
+            finalRepeatedString = fullStringRepeated + s.substring(0, Math.min(stringLen, nRemainder));
+        } else if (stringLen > n) {
+            finalRepeatedString = s.substring(0, Math.min(stringLen, (int)n));
+        } else {
+            finalRepeatedString = s;
+        }
 
+        int aCount = 0;
+        for (char character: finalRepeatedString.toCharArray()) {
+            if (character == 'a') {
+                aCount++;
+            }
+        }
+
+        return (long)aCount;
     }
 
     // More efficient solution
@@ -13,7 +35,7 @@ public class RepeatedString {
     }
 
     public static void main(String[] args) {
-        long result = repeatedString("abca", 10);
-
+        long result = repeatedString("a", 1000000000000L);
+        System.out.println("Count: " + result);
     }
 }
