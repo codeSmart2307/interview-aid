61b7d13b5e37041e9f4cf5a29809c80057661ae3
codeSmart2307 <raneesh2307@gmail.com>
2020-11-11 21:04:15 +0530

    add recursion

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -2,21 +2,25 @@ package algorithms.sorting;
 
 // Java program for implementation of Bubble Sort
 class BubbleSort {
-    void bubbleSort(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++)
-            for (int j = 0; j < arr.length - i - 1; j++)
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+    static void sort(int[] arr) {
+        int n = arr.length;
+        int temp;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
                 }
+
+            }
+        }
     }
 
     public static void main(String args[]) {
-        BubbleSort ob = new BubbleSort();
         int arr[] = {64, 34, 25, 12, 22, 11, 90};
-        ob.bubbleSort(arr);
+        BubbleSort.sort(arr);
         for (int value : arr) System.out.print(value + " ");
     }
 }
